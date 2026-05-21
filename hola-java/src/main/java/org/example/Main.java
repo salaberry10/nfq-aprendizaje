5752549a33d338dad6a51999bd0ea2daac4b39c4
Jose Manuel Salaberry Hidalgo <josemsalaberry@hotmail.com>
2026-05-21 13:20:18 +0200

    Semana 1: setup proyecto y ejercicios de variables (Sec. 1-2)

---
+++ hola-java/src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola, NFQ");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Como te llamas");
+        String nombre = sc.nextLine();
+
+        System.out.println("Cuantos años tienes");
+        int edad = sc.nextInt();
+
+        System.out.println("Hola " + nombre + " tienes " + edad + " años");
+    }
+
+}
